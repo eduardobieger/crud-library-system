95fed6fd2dc76ee885b38e0cd3959f7bf2818edf
eduardobieger <eduardo42bieger@gmail.com>
2023-07-05 12:30:58 -0300

    created book entity

---
+++ src/main/java/dev/bieger/model/entity/Book.java
@@ -0,0 +1,42 @@
+package dev.bieger.model.entity;
+
+public class Book {
+
+    private Long id;
+    private String title;
+    private Long authorId;
+    private Long genreId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public Long getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(Long genreId) {
+        this.genreId = genreId;
+    }
+
+}
