bde2a2257a3ce676c3b0d7736e2cd5c1db304aeb
eduardobieger <eduardo42bieger@gmail.com>
2023-07-05 12:31:11 -0300

    created book copy entity

---
+++ src/main/java/dev/bieger/model/entity/BookCopy.java
@@ -0,0 +1,51 @@
+package dev.bieger.model.entity;
+
+public class BookCopy {
+
+    private String isbn;
+    private String bookId;
+    private Long publisherId;
+    private int year;
+    private int status;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public Long getPublisherId() {
+        return publisherId;
+    }
+
+    public void setPublisherId(Long publisherId) {
+        this.publisherId = publisherId;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+}
